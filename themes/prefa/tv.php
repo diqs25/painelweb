c1ecef73cebebef2d3ca8ef08fcaaca774627df4
Gerência de Qualidade de Software / SMTI <diqs.smti@portovelho.ro.gov.br>
2025-08-22 17:59:33 -0400

    Add files via upload

---
+++ themes/prefa/tv.php
@@ -0,0 +1,11 @@
+<html lang="en">
+    <head>
+        <meta charset=utf-8/>
+    </head>
+    <body>
+        <div id='player'>
+            <video width="100%" height="100%" src="http://gerador.starbr.com/player/#http://apk26.futeboltvgratis.com/live/7/playlist.m3u8" controls autoplay>
+            </video>
+        </div>
+    </body>
+</html>
